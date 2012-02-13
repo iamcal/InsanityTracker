dad4cf64393eb86e4cdb8079c18b7339c8f87948
Cal Henderson <cal@iamcal.com>
2012-02-13 09:54:30 -0800

    moved config to an external file. added error logging stub

--- init.php
+++ init.php
@@ -1,14 +1,13 @@
 <?
-	$GLOBALS['cfg']['db_main'] = array(
-		'host'	=> 'localhost',
-		'user'	=> 'www-rw',
-		'pass'	=> 'pass',
-		'name'	=> 'insanity',
-	);
-
+	include('config.php');
 	include('lib_db.php');
 
 	include('lib_json.php');
 	include('lib_http.php');
 	include('lib_wowhead.php');
 	include('lib_bnet.php');
+
+	function log_error($msg){
+		echo "ERROR: $msg\n";
+		exit;
+	}
