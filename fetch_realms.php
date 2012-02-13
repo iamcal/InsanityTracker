f5930ebf6bceb7356f8b05aa51dca4619a9b7493
Cal Henderson <cal@iamcal.com>
2012-02-13 10:58:51 -0800

    fetch realms in multiple locales (so we get european russian realms). ignore china for now

--- fetch_realms.php
+++ fetch_realms.php
@@ -2,34 +2,62 @@
 	include('init.php');
 
 
-	function fetch_region($r){
+	function fetch_region($r, $l){
 
-		echo "updating $r ... ";
+		echo "updating $r $l ... ";
 		$num = 0;
 
-		$ret = bnet_make_request($r, "/realm/status");
+		$ret = bnet_make_request($r, "/realm/status?locale=$l");
 		if ($ret['ok']){
 
-			db_write("BEGIN");
-			db_write("DELETE FROM realms WHERE region='$r'");
 			foreach ($ret['data']['realms'] as $row){
 
-				db_insert('realms', array(
+				$GLOBALS['db_rows'][$r.'-'.$row['slug']] = array(
 					'region'	=> $r,
 					'slug'		=> AddSlashes($row['slug']),
 					'name'		=> AddSlashes($row['name']),
-				));
+				);
 
 				$num++;
 			}
-			db_write("COMMIT");
 		}
 
 		echo "found $num\n";
 	}
 
-	fetch_region('us');
-	fetch_region('eu');
-	fetch_region('kr');
-	fetch_region('tw');
-	fetch_region('cn');
+	function fetch_regions($region, $langs){
+		foreach ($langs as $lang){
+			fetch_region($region, $lang);
+		}
+	}
+
+
+	#
+	# find realms
+	#
+
+	$GLOBALS['db_rows'] = array();
+
+	fetch_regions('us', array('en_US', 'es_MX'));
+	fetch_regions('eu', array('en_GB', 'es_ES', 'fr_FR', 'ru_RU', 'de_DE'));
+	fetch_regions('kr', array('ko_KR'));
+	fetch_regions('tw', array('zh_TW'));
+	#fetch_regions('cn', array('zh_CN'));
+
+
+	#
+	# insert
+	#
+
+	echo "inserting ".count($db_rows)." realms ... ";
+
+	db_write("BEGIN");
+	db_write("DELETE FROM realms");
+
+	foreach ($db_rows as $row){
+		db_insert('realms', $row);
+	}
+
+	db_write("COMMIT");
+
+	echo "ok\n";
