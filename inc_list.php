71e639e3eded9f6a9459dca9e8335477e75ac147
Cal Henderson <cal@iamcal.com>
2012-02-28 14:47:42 -0800

    restructuring the URLs a bit

--- inc_list.php
+++ inc_list.php
@@ -16,8 +16,8 @@
 	$profile = "http://$host/wow/en/character/{$realm['slug']}/{$name_url}/";
 	$guild = "http://$host/wow/en/guild/{$realm['slug']}/{$guild_url}/?character={$name_url}";
 
-	$profile = "/insanity/character/{$realm['region']}/{$realm['slug']}/{$name_url}/";
-	$guild = "/insanity/guild/{$realm['region']}/{$realm['slug']}/{$guild_url}/"
+	$profile = "/insanity/{$realm['region']}/{$realm['slug']}/{$name_url}/";
+	$guild = "/insanity/guilds/{$realm['region']}/{$realm['slug']}/{$guild_url}/"
 ?>
 	<tr>
 		<td><a href="<?=$profile?>"><?=HtmlSpecialChars($row['name'])?></a></td>
