b95d53b16d0c6a2a1afc4386b084024a91553d12
Cal Henderson <cal@iamcal.com>
2012-02-28 16:47:25 -0800

    first stab at the character page

--- character.php
+++ character.php
@@ -13,6 +13,22 @@
 		die('character not found');
 	}
 
+	$guild_enc = AddSlashes($character['guild']);
+	$guild = db_single(db_fetch("SELECT * FROM guilds WHERE region='$character[region]' AND realm='$realm_enc' AND name='$guild_enc'"));
+	if ($guild['region']){
+
+		$realm_url = rawurlencode($guild['realm']);
+		$name_url = rawurlencode($guild['name']);
+
+		$guild['url'] = "/insanity/guilds/$guild[region]/$realm_url/$name_url/";
+	}
+
+
+	$host = $GLOBALS['cfg']['bnet_region_hosts'][$realm['region']];
+	$realm_url = rawurlencode($character['realm']);
+	$name_url = rawurlencode($character['name']);
+	$armory = "http://$host/wow/en/character/{$realm_url}/{$name_url}/";
+
 	include('head.txt');
 ?>
 
@@ -26,7 +42,43 @@
 	<?=HtmlSpecialChars($character['name'])?>
 </h1>
 
-<? dumper($character); ?>
+<table border="1">
+	<tr>
+		<td>Class</td>
+		<td><?=$character['class_id']?></td>
+	</tr>
+	<tr>
+		<td>Race</td>
+		<td><?=$character['race_id']?></td>
+	</tr>
+	<tr>
+		<td>Achievements</td>
+		<td><?=$character['achievement_points']?></td>
+	</tr>
+	<tr>
+		<td>Insane</td>
+		<td><?=$character['got_it']?> / <?=$character['date_got']?></td>
+	</tr>
+	<tr>
+		<td>Armory</td>
+		<td><a href="<?=$armory?>">Link</a>
+	</tr>
+</table>
+
+&nbsp;<br />
+
+<div class="statbox">
+<? if ($character['guild'] && $guild['total_got']){ ?>
+	This player is in the guild <a href="<?=$guild['url']?>"><?=HtmlSpecialChars($character['guild'])?></a>.<br />
+	<br />
+	Total active: <?=$guild['total_found']?><br />
+	Total insane: <?=$guild['total_got']?><br />
+<? }elseif ($character['guild']){ ?>
+	This player is in the guild <a href="<?=$guild['url']?>"><?=HtmlSpecialChars($character['guild'])?></a>. This guild is currently unranked.
+<? }else{ ?>
+	This player is not currently in a guild.
+<? } ?>
+</div>
 
 <?
 	include('foot.txt');
