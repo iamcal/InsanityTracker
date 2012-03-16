56b293ac76d1cbd9fd6b03bdd32b542e6543a120
Cal Henderson <cal@iamcal.com>
2012-03-15 17:31:10 -0700

    added better explanation on homepage

--- index.php
+++ index.php
@@ -43,7 +43,9 @@
 
 		This site tracks players who gain the achievement <a href="http://www.wowhead.com/achievement=2336">Insane in the Membrane</a>,
 		arguably the hardest task in World of Warcraft. Earning the prestigious 'Insane' title takes months of dedicated play, for
-		essentially no reward.
+		essentially no reward.<br />
+		<br />
+		This website tracks which players have earned it and when.
 
 	</div>
 
