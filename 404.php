b019daec240c3345e6fe2f29e5867ae66f584fc6
Cal Henderson <cal@iamcal.com>
2012-03-16 15:57:16 -0700

    auto-slash 404s

--- 404.php
+++ 404.php
@@ -1,6 +1,27 @@
 <?
 	include('init.php');
 
+
+	#
+	# try adding a slash at the end if:
+	# 1) we've not already mapped it through a RewriteRule
+	# 2) it doesn't look like a filename
+	# 3) it doesn't already have a slash at the end
+	#
+
+	$url  = $_SERVER['REQUEST_URI'];
+	$orig = $_SERVER['REDIRECT_URL'];
+
+	if ($url == $orig){
+		$last_part = array_pop((explode('/', $url)));
+		if (preg_match('!^[^\.]+$!', $last_part)){
+
+			header("location: $url/");
+			exit;
+		}
+	}
+
+
 	$error = "The page you requested could not be found.";
 	include('notfound.php');
 ?>
